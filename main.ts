b0d3320750069be0babd71e3b14d1b24dfb68b3c
kotaindah55 <kotaindah55@gmail.com>
2024-09-27 22:13:31 +0300

    put state fields and view plugins together in one extension array

--- main.ts
+++ main.ts
@@ -1,36 +1,35 @@
 import { Plugin } from "obsidian";
+import { EditorView } from "@codemirror/view";
 // import { drawSelection } from "@codemirror/view";
 import { extendedFormattingPlugin, customHighlightPlugin, alignerPlugin } from "./src/editorPlugins/decorationBuilders";
 import { customHighlightField, extendedFormattingField } from "./src/editorPlugins/stateFields";
 import { ExtendedFormattingPostProcessor, CustomHighlightPostProcessor, AlignerPostProcessor } from "./src/postProcessor";
 
 export default class ExtendedMarkdownSyntax extends Plugin {
-
+    
     async onload() {
         await this.loadData();
-
-        // state fields
+          
         this.registerEditorExtension([
+            // state fields
             extendedFormattingField,
-            customHighlightField
-        ]);
-
-        // view plugins
-        this.registerEditorExtension([
+            customHighlightField,
+            // view plugins
             extendedFormattingPlugin,
             customHighlightPlugin,
             alignerPlugin,
-            // drawSelection()
+            // facet
+            EditorView.outerDecorations.of(view => view.plugin(customHighlightPlugin)!.decorations)
         ]);
-
+        
         // postprocessor
         this.registerMarkdownPostProcessor(new ExtendedFormattingPostProcessor(this.app.workspace).postProcess);
         this.registerMarkdownPostProcessor(new CustomHighlightPostProcessor().postProcess);
         this.registerMarkdownPostProcessor(new AlignerPostProcessor().postProcess);
-
+        
         console.log("Load Extended Markdown Syntax");
     }
-
+    
     onunload(): void {
         console.log("Unload Extended Markdown Syntax");
     }
